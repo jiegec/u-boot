d61fa1197410537b7fe71378204eee30fff892b1
Jiajie Chen <c@jia.je>
2021-10-17 09:55:17 +0800

    Update comments of cflush handling

--- board/custom/rocket-chip-vcu128/rocket-chip-vcu128.c
+++ board/custom/rocket-chip-vcu128/rocket-chip-vcu128.c
@@ -10,6 +10,9 @@ int board_late_init(void)
 	return 0;
 }
 
+// cflush.d.l1 is only usable in S-mode
+// so we emulate it in opensbi
+
 // code is taken from https://blog.csdn.net/a_weiming/article/details/116090948
 #define STR1(x) #x
 #ifndef STR
@@ -33,7 +36,7 @@ int board_late_init(void)
 #define FLUSH_D_REG(rs1) CFLUSH_D_L1_INST(rs1, 13)
 
 //rs1 is data
-//rs_1 si the register number to use
+//rs_1 is the register number to use
 #define CFLUSH_D_L1_INST(rs1, rs1_n)                                                        \
 	{                                                                                   \
 		register uint32_t rs1_ asm("x" #rs1_n) = (uint32_t)rs1;                     \
@@ -49,13 +52,10 @@ void flush_dcache_all(void)
 
 void flush_dcache_range(unsigned long start, unsigned long end)
 {
-	//printf("Flush DCache From: %lx\n", start);
-	//printf("Flush DCache To: %lx\n", end);
 	start = (start >> 6) * 64;
 	end = (end >> 6) * 64;
 	for (unsigned long addr = start; addr <= end; addr = addr + 64)
 	{
-		//printf("Flush DCache: %lx\n", addr);
 		FLUSH_D_REG(addr);
 	}
 }
