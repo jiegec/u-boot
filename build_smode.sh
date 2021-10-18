10fc4758aab80de61d1d831910eae6aae6a089a2
Jiajie Chen <c@jia.je>
2021-10-18 08:22:07 +0800

    Reduce image size and add build script

---
+++ build_smode.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+make CROSS_COMPILE=riscv64-linux-gnu- -j8 rocket-chip-vcu128-smode_defconfig
+make CROSS_COMPILE=riscv64-linux-gnu- -j8
+cd $HOME/opensbi
+make CROSS_COMPILE=riscv64-linux-gnu- -j4 PLATFORM=rocket-chip-vcu128 FW_PAYLOAD_PATH=$HOME/u-boot/u-boot.bin
