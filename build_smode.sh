d89ea5f98c3e8a6937d45d8772d842f078d2299c
Jiajie Chen <c@jia.je>
2023-02-22 12:48:18 +0800

    Reduce binary size to 237K

--- build_smode.sh
+++ build_smode.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 make CROSS_COMPILE=riscv64-linux-gnu- -j8 rocket-chip-vcu128-smode_defconfig
 make CROSS_COMPILE=riscv64-linux-gnu- -j8
+size u-boot
+ls -alh u-boot.bin
 cd $HOME/opensbi
 make CROSS_COMPILE=riscv64-linux-gnu- -j4 PLATFORM=rocket-chip-vcu128 FW_PAYLOAD_PATH=$HOME/u-boot/u-boot.bin
