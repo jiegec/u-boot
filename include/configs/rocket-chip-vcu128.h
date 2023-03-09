022b1b9850fb74f54951d72224a1694b4131a28d
Jiajie Chen <c@jia.je>
2023-03-09 18:34:25 +0800

    Split single and dual core

--- include/configs/rocket-chip-vcu128.h
+++ include/configs/rocket-chip-vcu128.h
@@ -17,7 +17,10 @@
   "serverip=10.0.0.1\0"                                                        \
   "ipaddr=10.0.0.2\0"                                                          \
   "netmask=255.255.255.0\0"                                                    \
-  "boot_linux=tftpboot 0x80100000 10.0.0.1:image.itb;bootm 0x80100000\0"
+  "boot_single=tftpboot 0x80100000 10.0.0.1:image-single-core.itb;bootm "      \
+  "0x80100000\0"                                                               \
+  "boot_dual=tftpboot 0x80100000 10.0.0.1:image-dual-core.itb;bootm "          \
+  "0x80100000\0"
 
 #define CONFIG_TIMESTAMP
 
