3bb604a8c4e43809d5644b942c0ed81b6b1d4952
Jiajie Chen <c@jia.je>
2023-03-09 17:36:22 +0800

    Build for single core platform

--- build_smode.sh
+++ build_smode.sh
@@ -4,4 +4,5 @@ make CROSS_COMPILE=riscv64-linux-gnu- -j8
 size u-boot
 ls -alh u-boot.bin u-boot-nodtb.bin
 cd $HOME/opensbi
+make CROSS_COMPILE=riscv64-linux-gnu- -j4 PLATFORM=rocket-chip-vcu128-single-core FW_PAYLOAD_PATH=$HOME/u-boot/u-boot-nodtb.bin
 make CROSS_COMPILE=riscv64-linux-gnu- -j4 PLATFORM=rocket-chip-vcu128-dual-core FW_PAYLOAD_PATH=$HOME/u-boot/u-boot-nodtb.bin
