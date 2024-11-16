c84955c887a5ddffbd8608818533744627c81b4a
Jiajie Chen <c@jia.je>
2024-11-16 10:42:24 +0800

    Sync code with qemu riscv64

--- include/configs/rocket-chip-vcu128.h
+++ include/configs/rocket-chip-vcu128.h
@@ -3,14 +3,12 @@
 
 #include <linux/sizes.h>
 
-#define CONFIG_SYS_SDRAM_BASE 0x80000000
+#define CFG_SYS_SDRAM_BASE 0x80000000
 
 #define RISCV_MMODE_TIMERBASE 0x2000000
 #define RISCV_MMODE_TIMER_FREQ 1000000
 
-#define CONFIG_ENV_IS_NOWHERE 1
-
-#define CONFIG_EXTRA_ENV_SETTINGS                                              \
+#define CFG_EXTRA_ENV_SETTINGS                                              \
   "autoload=no\0"                                                              \
   "serverip=10.0.0.1\0"                                                        \
   "ipaddr=10.0.0.2\0"                                                          \
@@ -19,7 +17,5 @@
   "0x80100000\0"                                                               \
   "boot_dual=tftpboot 0x80100000 10.0.0.1:image-dual-core.itb;bootm "          \
   "0x80100000\0"
-
-#define CONFIG_TIMESTAMP
 
 #endif /* __CONFIG_H */
